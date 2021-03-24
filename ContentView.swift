c87f5e86fd8975c921e11df0ba55a2b489b10842
Katie <25576005+stuttskl@users.noreply.github.com>
2021-03-24 14:28:00 -0700

    added single page view of task list

--- ContentView.swift
+++ ContentView.swift
@@ -1,16 +1,71 @@
-//
-//  ContentView.swift
-//  NightWatch
-//
-//  Created by Katie Stutts on 3/23/21.
-//
 
 import SwiftUI
 
 struct ContentView: View {
+    let nightlyTasks = ["Check all windows", "Check all doors", "Check that the safe is locked", "Inspect security cameras", "Clear ice from sidewalks", "Document \"strange and unusual\" occurrences"]
+    let weeklyTasks = ["Check inside all vacant rooms", "Walk the perimiter of the property"]
+    
+    let monthlyTasks = ["Test security alarm", "Test motion detectors", "Test smoke alarms"]
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            HStack {
+                VStack(alignment: .leading) {
+                    // MARK: Nightly tasks
+                    List {
+                        Section(header: HStack {
+                            Text(Image(systemName: "moon.stars"))
+                                .foregroundColor(.yellow)
+                                .font(.title3)
+                                .fontWeight(.heavy)
+                            Text("Nightly Tasks")
+                                .underline()
+                                .font(.title3)
+                                .foregroundColor(.yellow)
+                                .fontWeight(.heavy)
+                                .textCase(/*@START_MENU_TOKEN@*/.uppercase/*@END_MENU_TOKEN@*/)
+                        }) {
+                            ForEach(nightlyTasks, id: \.self, content: {
+                                taskName in Text(taskName)
+                            })
+                        }
+                        Section(header: HStack {
+                            Text(Image(systemName: "sunrise"))
+                                .foregroundColor(.yellow)
+                                .font(.title3)
+                                .fontWeight(.heavy)
+                            Text("Weekly Tasks")
+                                .underline()
+                                .font(.title3)
+                                .foregroundColor(.yellow)
+                                .fontWeight(.heavy)
+                                .textCase(/*@START_MENU_TOKEN@*/.uppercase/*@END_MENU_TOKEN@*/)
+                        }) {
+                            ForEach(weeklyTasks, id: \.self, content: {
+                                taskName in Text(taskName)
+                            })
+                        }
+                        Section (header: HStack {
+                            Text(Image(systemName: "calendar"))
+                                .foregroundColor(.yellow)
+                                .font(.title3)
+                                .fontWeight(.heavy)
+                            Text("Monthly Tasks")
+                                .underline()
+                                .font(.title3)
+                                .foregroundColor(.yellow)
+                                .fontWeight(.heavy)
+                                .textCase(/*@START_MENU_TOKEN@*/.uppercase/*@END_MENU_TOKEN@*/)
+                        }) {
+                            ForEach(monthlyTasks, id: \.self, content: {
+                                taskName in Text(taskName)
+                            })
+                        }
+                    }.listStyle(GroupedListStyle())
+
+            }
+            Spacer()
+        }
+
     }
 }
 
@@ -18,4 +73,5 @@ struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
+}
 }
